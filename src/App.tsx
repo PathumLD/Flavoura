3d4601d88587b6fb5525db9601c6271d0ab5301c
PathumLD <pathumlk10158@gmail.com>
2025-04-29 00:52:05 +0530

    Updated app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Reservations from './pages/Reservations';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ScrollToTop from './components/utils/ScrollToTop';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/reservations" element={<Reservations />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
